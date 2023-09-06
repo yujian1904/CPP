f5763fbaee5991f40ceea96bae6c9419a1a313b4
yujian1904 <yujian1904@outlook.com>
2023-09-06 21:22:40 +0800

    20230906-006

--- cmake-build-debug/CMakeFiles/3.25.2/CMakeRCCompiler.cmake
+++ cmake-build-debug/CMakeFiles/3.25.2/CMakeRCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_RC_COMPILER "C:/developTools/CLion 2023.2.1/bin/mingw/bin/windres.exe")
+set(CMAKE_RC_COMPILER "C:/Development Tools/CLion 2023.1.4/bin/mingw/bin/windres.exe")
 set(CMAKE_RC_COMPILER_ARG1 "")
 set(CMAKE_RC_COMPILER_LOADED 1)
 set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
