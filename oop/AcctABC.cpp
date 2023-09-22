d48875b75b796643b3cc013cba172d7d3bdac509
yujian1904 <yujian1904@outlook.com>
2023-09-22 20:26:15 +0800

    20230920-001

---
+++ oop/AcctABC.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dcfhospitalcgb on 2023/9/20.
+//
+
+#include "AcctABC.h"
---
+++ oop/AcctABC.h
@@ -0,0 +1,19 @@
+//
+// Created by dcfhospitalcgb on 2023/9/20.
+//
+
+#ifndef CPP_ACCTABC_H
+#define CPP_ACCTABC_H
+
+
+class AcctABC {
+private:
+
+public:
+    AcctABC();
+
+    ~AcctABC();
+};
+
+
+#endif //CPP_ACCTABC_H
